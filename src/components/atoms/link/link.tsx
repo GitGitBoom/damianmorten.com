0263b0a4414e619f16e41a3fb9ac8d06e7ddbce8
Damian Morten <damianmorten@gmail.com>
2021-03-25 12:16:09 -0400

    feat: make 'Link' component flex

--- src/components/atoms/link/link.tsx
+++ src/components/atoms/link/link.tsx
@@ -2,11 +2,14 @@ import { Link as BaseLink } from '@chakra-ui/react'
 import styled from '@emotion/styled'
 import { HTMLChakraProps } from '@chakra-ui/react'
 
-export type Props = HTMLChakraProps<'a'> 
+export type Props = HTMLChakraProps<'a'>
 
 export const Link = styled(BaseLink)`
   flex: 1;
-  &:focus, &:active {
+  display: inline-flex;
+  flex-directon: column;
+  &:focus,
+  &:active {
     box-shadow: 0 0 0;
     outline: 0;
   }
