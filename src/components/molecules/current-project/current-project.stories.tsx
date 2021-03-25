81572fe8d3217daf420395304109bd615e5060c3
Damian Morten <damianmorten@gmail.com>
2021-03-25 11:33:56 -0400

    feat: complete 'CurrentProject' component

--- src/components/molecules/current-project/current-project.stories.tsx
+++ src/components/molecules/current-project/current-project.stories.tsx
@@ -1,12 +1,19 @@
-import { CurrentProject, Props } from './current-project';
-import type { Story, Meta } from '@storybook/react/types-6-0';
+import { CurrentProject, Props } from './current-project'
+import type { Story, Meta } from '@storybook/react/types-6-0'
 
 export default {
   title: 'Molecules/CurrentProject',
   component: CurrentProject,
 } as Meta
 
-const Template: Story<Props> = (args) => <CurrentProject {...args} />;
+const Template: Story<Props> = (args) => <CurrentProject {...args} />
 
-export const Default = Template.bind({});
-Default.args = {}
+export const Default = Template.bind({})
+Default.args = {
+  link: 'https://about.gitlab.com/',
+  title: 'A new project',
+  image: 'https://via.placeholder.com/140x100',
+  bg: 'facebook.400',
+  width: 300,
+  height: 300,
+}
--- src/components/molecules/current-project/current-project.tsx
+++ src/components/molecules/current-project/current-project.tsx
@@ -1,31 +1,25 @@
-import { Text, Center } from '@chakra-ui/react'
+import { Text } from '@chakra-ui/react'
 import { FlappyBox } from '@/atoms/flappy-box'
 import { Link } from '@/atoms/link'
 import type { Props as FlappyBoxProps } from '@/components/atoms/flappy-box'
 import type { Me } from '@/graphql/me-type'
 
-export interface Props extends FlappyBoxProps {
-  project: Required<Me>["currentProject"]
-}
+export type Props = FlappyBoxProps & Required<Me>['currentProject']
 export const CurrentProject: React.FC<Props> = (props) => {
-  const {project, ...boxProps} = props;
-  const {title, link, image} = project;
+  const { title, link, image, ...boxProps } = props
   return (
     <Link href={link} isExternal>
-      <FlappyBox py={10} bg="cyan.900" {...boxProps}>
-        <Text
-          marginY={2}
-          textTransform="uppercase"
-          textAlign="center"
-          fontSize="xs"
-          >Currently working with</Text>
-        <Center p={2} flex={1}>
-          <img
-            src={image}
-            width={80}
-            title={title}
-            />
-        </Center>
+      <FlappyBox
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+        {...boxProps}
+      >
+        <Text mb={2} textTransform="uppercase" textAlign="center" fontSize="xs">
+          Currently working with
+        </Text>
+        <img src={image} width={80} title={title} />
       </FlappyBox>
     </Link>
   )
