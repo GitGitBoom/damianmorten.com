a27f9682210e2d985edfe15d54df6aff163ccaec
Damian Morten <damianmorten@gmail.com>
2021-03-25 11:36:31 -0400

    feat: complete 'LatestGithub' component

--- src/components/molecules/latest-github/latest-github.stories.tsx
+++ src/components/molecules/latest-github/latest-github.stories.tsx
@@ -1,11 +1,20 @@
-import { LatestGithub } from './latest-github';
-import type { Story, Meta } from '@storybook/react/types-6-0';
+import { LatestGithub, Props } from './latest-github'
+import type { Story, Meta } from '@storybook/react/types-6-0'
 
 export default {
   title: 'Molecules/LatestGithub',
   component: LatestGithub,
 } as Meta
 
-const Template: Story = (args) => <LatestGithub {...args} />;
+const Template: Story<Props> = (args) => <LatestGithub {...args} />
 
-export const Default = Template.bind({});
+export const Default = Template.bind({})
+Default.args = {
+  type: 'CommitCommentEvent',
+  link: 'https://storybook.js.org',
+  bg: 'gray.700',
+  color: 'white',
+  fontSize: 60,
+  width: 300,
+  height: 300,
+}
--- src/components/molecules/latest-github/latest-github.tsx
+++ src/components/molecules/latest-github/latest-github.tsx
@@ -1,67 +1,56 @@
-import { Flex} from '@chakra-ui/react'
-// import { Octokit } from '@octokit/rest'
+import { Text, Box } from '@chakra-ui/react'
+import { Link } from '@/atoms/link'
+import { FlappyBox } from '@/atoms/flappy-box'
+import type { Props as FlappyBoxProps } from '@/atoms/flappy-box'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { GithubEventTypes } from '@/graphql/me-type'
 
-// import { FlappyBox } from '@/components/atoms/flappy-box';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import {faStackOverflow, faGithub} from '@fortawesome/free-brands-svg-icons'
-// import {faEnvelope, faList} from '@fortawesome/free-solid-svg-icons'
-
-/**
- * Reference: https://docs.github.com/en/developers/webhooks-and-events/github-event-types
- */
-// enum GithubEvent {
-//   // Comment on a commit
-//   CommitCommentEvent = 'CommitCommentEvent',
-//   // Create a new branch or tag
-//   CreateEvent = 'CreateEvent',
-//   // Delete a branch or tag
-//   DeleteEvent = 'DeleteEvent',
-//   // Fork a repo
-//   ForkEvent = 'ForkEvent',
-//   // Create or update a wiki page
-//   GollumEvent = 'GollumEvent',
-//   // Comment on an issue
-//   IssueCommentEvent = 'IssueCommentEvent',
-//   // Create, update, lock, unlock an issue
-//   IssuesEvent = 'IssuesEvent',
-//   // Add/Remove a collaborator
-//   MemberEvent = 'MemberEvent',
-//   // Made a private repo public
-//   PublicEvent = 'PublicEvent',
-//   // Submitted a pull request
-//   PullRequestEvent = 'PullRequestEvent',
-//   // Commented on a pull request
-//   PullRequestReviewCommentEvent = 'PullRequestReviewCommentEvent',
-//   // Pushed commits
-//   PushEvent = 'PushEvent',
-//   // Released a version
-//   ReleaseEvent = 'ReleaseEvent',
-//   // Got sponsored
-//   SponsorshipEvent = 'SponsorshipEvent',
-//   // Watched a repo
-//   WatchEvent = 'WatchEvent'
-// }
-
-// const renderEvent = (type: GithubEvent): React.ReactNode => {
-//   switch (type) {
-//     case GithubEvent.CommitCommentEvent:  
-//       return (
-//         <Link>Commented on a commit</Link>
-//       );
-//     case GithubEvent.CreateEvent:  
-//       return (
-//         <Link>Created a </Link>
-//       );
-//   }
-// }
-
-export interface Props {
-  // time: number
+const EventMessageMap = {
+  CommitCommentEvent: 'Commented on a commit',
+  CreateEvent: 'Created a new branch',
+  DeleteEvent: 'Deleted a branch',
+  ForkEvent: 'Forked a repo',
+  GollumEvent: 'Edited a wiki page',
+  IssueCommentEvent: 'Commented on an issue',
+  IssuesEvent: 'Edited an issue',
+  MemberEvent: 'Add/Removed a collaborator',
+  PublicEvent: 'Made a private repo public',
+  PullRequestEvent: 'Submitted a PR',
+  PullRequestReviewCommentEvent: 'Commented on a PR',
+  PushEvent: 'Pushed to a public repo',
+  ReleaseEvent: 'Released a new version',
+  SponsorshipEvent: 'Got sponsored!',
+  WatchEvent: 'Watched a repo',
 }
-export const LatestGithub: React.FC<Props> = () => {
+
+export interface Props extends FlappyBoxProps {
+  type: keyof GithubEventTypes
+  link: string
+}
+export const LatestGithub: React.FC<Props> = (props) => {
+  const { type, link, ...boxProps } = props
   return (
-    <Flex>
-      foo
-    </Flex>
+    <Link href={link} isExternal>
+      <FlappyBox
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+        {...boxProps}
+      >
+        <Text mb={2} textTransform="uppercase" textAlign="center" fontSize="xs">
+          Latest Action
+        </Text>
+
+        <Box fontSize={75}>
+          <FontAwesomeIcon icon={faGithub} />
+        </Box>
+
+        <Text textAlign="center" fontSize="xl" mt={2}>
+          {EventMessageMap[type]}
+        </Text>
+      </FlappyBox>
+    </Link>
   )
 }
