3ed477e370edff4051bcf85693bfa45a73cb92f7
Damian Morten <damianmorten@gmail.com>
2021-04-01 13:04:10 -0400

    fix(Hero): add zIndex config to hero layers to correct 'flashing' render bug on Safari

--- src/components/organisms/hero/hero.stories.tsx
+++ src/components/organisms/hero/hero.stories.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { Hero } from '@/organisms/hero';
-import type { Story, Meta } from '@storybook/react/types-6-0';
+import React from 'react'
+import { Hero } from '@/organisms/hero'
+import type { Story, Meta } from '@storybook/react/types-6-0'
 
 export default {
   title: 'Organisms/Hero',
   component: Hero,
   parameters: {
     backgrounds: {
-      default: 'dark'
-    }
-  }
+      default: 'dark',
+    },
+  },
 } as Meta
 
-const Template: Story = (args) => <Hero {...args} />;
+const Template: Story = (args) => <Hero {...args} />
 
-export const Default = Template.bind({});
+export const Default = Template.bind({})
